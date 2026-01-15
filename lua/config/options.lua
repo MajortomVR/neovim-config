e397445a6fe61f2c54af574ca88d84abd86317ba
MajortomVR <majortom.rb@gmail.com>
2026-01-15 23:06:56 +0100

    Added further options and comments

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,8 @@
+-- Basic Settings
 vim.opt.number = true -- enable line numbers
-vim.opt.cursorline = true -- enable cursor line
 vim.opt.relativenumber = true -- enable relative line numbers
+vim.opt.cursorline = true -- enable cursor line
 vim.opt.scrolloff = 8 -- always keep 8 lines above/below the cursor unless at start/end of file
-vim.opt.signcolumn = "yes" -- always show sign column
-vim.opt.termguicolors = true -- enable 24-bit colors
 
 -- Indentation
 vim.opt.shiftwidth = 4 -- contols number of spaces when using >> or << commands
@@ -11,6 +10,16 @@ vim.opt.tabstop = 4 -- how many spaces tab inserts
 vim.opt.softtabstop = 4 -- how many spaces tab inserts
 vim.opt.expandtab = true -- use appropriate number of spaces with tab
 vim.opt.smartindent = true -- indenting correctly after {
+vim.opt.autoindent = true -- copy indent from current line
 
 -- Copy/Paste to clipboard
 vim.opt.clipboard = "unnamedplus"
+
+-- Visual Settings
+vim.opt.termguicolors = true -- enable 24-bit colors
+vim.opt.signcolumn = "yes" -- always show sign column
+
+-- Search settings
+vim.opt.ignorecase = true -- Case insensitive search
+vim.opt.smartcase = true -- Case sensitive if uppercase in search
+vim.opt.incsearch = true -- Show matches as you type
