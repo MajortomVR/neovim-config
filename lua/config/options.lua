dd26d2c02cd13647702584cdd32a245dc931c625
MajortomVR <majortom.rb@gmail.com>
2026-01-15 20:53:06 +0100

    fix intentation (tabs) and add copy/paste to clipboard

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -4,5 +4,9 @@ vim.opt.relativenumber = true
 
 -- Indentation
 vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.expandtab = true
 
-
+-- Copy/Paste to clipboard
+vim.opt.clipboard = "unnamedplus"
